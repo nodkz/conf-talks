151d67a0ee2df62a55293ca759de3c5f46196927
nodkz <pavel.chertorogov@gmail.com>
2018-09-13 23:52:08 +0600

    graphql: describe InterfaceType, List, NonNull, add test-case for UnionType

---
+++ particles/graphql/types/__tests__/interfaceType-test.js
@@ -0,0 +1,128 @@
+/* @flow strict */
+
+import {
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInterfaceType,
+  GraphQLList,
+  graphql,
+} from 'graphql';
+
+describe('check how works GraphQLInterfaceType', () => {
+  it('demo query', async () => {
+    class Event {
+      ip: string;
+      createdAt: number;
+      constructor({ ip, createdAt }: any) {
+        this.ip = ip;
+        this.createdAt = createdAt;
+      }
+    }
+
+    // Define our models
+    class ClickEvent extends Event {
+      url: string;
+      constructor(data) {
+        super(data);
+        this.url = data.url;
+      }
+    }
+
+    class SignedUpEvent extends Event {
+      login: string;
+      constructor(data) {
+        super(data);
+        this.login = data.login;
+      }
+    }
+
+    // Define out interface
+    const EventInterface = new GraphQLInterfaceType({
+      name: 'EventInterface',
+      fields: () => ({
+        ip: {
+          type: GraphQLString,
+          resolve: () => '*.*.*.*', // resolve method in Interface does not change your data in response
+        },
+        createdAt: { type: GraphQLInt },
+      }),
+      resolveType: value => {
+        if (value instanceof ClickEvent) {
+          return 'ClickEvent';
+        } else if (value instanceof SignedUpEvent) {
+          return 'SignedUpEvent';
+        }
+        return null;
+      },
+    });
+
+    // Define GraphQL types for our models
+    const ClickEventType = new GraphQLObjectType({
+      name: 'ClickEvent',
+      interfaces: [EventInterface],
+      fields: () => ({
+        ip: { type: GraphQLString },
+        createdAt: { type: GraphQLInt },
+        url: { type: GraphQLString },
+      }),
+    });
+
+    const SignedUpEventType = new GraphQLObjectType({
+      name: 'SignedUpEvent',
+      interfaces: [EventInterface],
+      fields: () => ({
+        ip: { type: GraphQLString },
+        createdAt: { type: GraphQLInt },
+        login: { type: GraphQLString },
+      }),
+    });
+
+    // Define some working schema with mock data
+    const schema = new GraphQLSchema({
+      types: [ClickEventType, SignedUpEventType],
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          search: {
+            type: new GraphQLList(EventInterface),
+            resolve: () => [
+              new ClickEvent({ ip: '1.1.1.1', createdAt: 1536854101, url: '/list' }),
+              new ClickEvent({ ip: '1.1.1.1', createdAt: 1536854102, url: '/register' }),
+              new SignedUpEvent({ ip: '1.1.1.1', createdAt: 1536854103, login: 'NICKNAME' }),
+              { some: 'strange object' },
+            ],
+          },
+        },
+      }),
+    });
+
+    const res = await graphql({
+      schema,
+      source: `
+        query {
+          search {
+            __typename  # <----- магическое поле, которое вернет имя типа для каждой записи
+            ip
+            createdAt
+            ...on ClickEvent {
+              url
+            }
+            ...on SignedUpEvent {
+              login
+            }
+          }
+        }
+      `,
+    });
+    expect(res.data).toEqual({
+      search: [
+        { __typename: 'ClickEvent', createdAt: 1536854101, ip: '1.1.1.1', url: '/list' },
+        { __typename: 'ClickEvent', createdAt: 1536854102, ip: '1.1.1.1', url: '/register' },
+        { __typename: 'SignedUpEvent', createdAt: 1536854103, ip: '1.1.1.1', login: 'NICKNAME' },
+        null, // <------- object with wrong type will be returned as null
+      ],
+    });
+  });
+});
--- particles/graphql/types/__tests__/unionType-test.js
+++ particles/graphql/types/__tests__/unionType-test.js
@@ -6,19 +6,154 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLUnionType,
+  GraphQLList,
   graphql,
 } from 'graphql';
 
 describe('check how works GraphQLUnionType', () => {
-  it('scalar types cannot be used with UnionType', async () => {
+  it('demo query', async () => {
+    // Define our models
+    class Article {
+      title: string;
+      publishDate: string;
+
+      constructor(title, publishDate) {
+        this.title = title;
+        this.publishDate = publishDate;
+      }
+    }
+
+    class Comment {
+      text: string;
+      author: string;
+
+      constructor(text, author) {
+        this.text = text;
+        this.author = author;
+      }
+    }
+
+    class UserProfile {
+      nickname: string;
+      age: number;
+
+      constructor(nickname, age) {
+        this.nickname = nickname;
+        this.age = age;
+      }
+    }
+
+    // Define GraphQL types for our models
+    const ArticleType = new GraphQLObjectType({
+      name: 'Article',
+      fields: () => ({
+        title: { type: GraphQLString },
+        publishDate: { type: GraphQLString },
+      }),
+    });
+
+    const CommentType = new GraphQLObjectType({
+      name: 'Comment',
+      fields: () => ({
+        text: { type: GraphQLString },
+        author: { type: GraphQLString },
+      }),
+    });
+
+    const UserProfileType = new GraphQLObjectType({
+      name: 'UserProfile',
+      fields: () => ({
+        nickname: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      }),
+    });
+
+    // Create our Union type which returns different ObjectTypes
+    const SearchRowType = new GraphQLUnionType({
+      name: 'SearchRow',
+      description:
+        'Search item which can be one of the following types: Article, Comment, UserProfile',
+      types: () => [ArticleType, CommentType, UserProfileType],
+      resolveType: value => {
+        if (value instanceof Article) {
+          return ArticleType;
+        } else if (value instanceof Comment) {
+          return CommentType;
+        } else if (value instanceof UserProfile) {
+          return UserProfileType;
+        }
+        return null;
+      },
+    });
+
+    // Define some working schema with mock data
+    const schema = new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          search: {
+            type: new GraphQLList(SearchRowType),
+            args: {
+              q: { type: GraphQLString },
+            },
+            resolve: () => [
+              new Article('Article 1', '2018-09-10'),
+              new Comment('Comment 1', 'Author 1'),
+              new UserProfile('Nick 1', 20),
+              new Article('Article 2', '2018-01-12'),
+              new Comment('Comment 2', 'Author 2'),
+              { some: 'strange object' },
+            ],
+          },
+        },
+      }),
+    });
+
+    const res = await graphql({
+      schema,
+      source: `
+        query {
+          search(q: "text") {
+            __typename # <----- магическое поле, которое вернет имя типа для каждой записи
+            ...on Article {
+              title
+              publishDate
+            }
+            ...on Comment {
+              text
+              author
+            }
+            ...on UserProfile {
+              nickname
+              age
+            }
+          }
+        }
+      `,
+    });
+    expect(res.data).toEqual({
+      search: [
+        { __typename: 'Article', publishDate: '2018-09-10', title: 'Article 1' },
+        { __typename: 'Comment', author: 'Author 1', text: 'Comment 1' },
+        { __typename: 'UserProfile', age: 20, nickname: 'Nick 1' },
+        { __typename: 'Article', publishDate: '2018-01-12', title: 'Article 2' },
+        { __typename: 'Comment', author: 'Author 2', text: 'Comment 2' },
+        null, // <------- object with wrong type will be returned as null
+      ],
+    });
+  });
+
+  it('scalar types CANNOT be used with UnionType', async () => {
     const ScalarUnionType = new GraphQLUnionType({
       name: 'ScalarUnion',
       description: '',
-      types: () => [GraphQLInt, GraphQLString],
+      types: (): any => [GraphQLInt, GraphQLString],
       resolveType: value => {
         if (typeof value === 'string') {
+          // $FlowFixMe
           return GraphQLString;
         } else if (typeof value === 'number') {
+          // $FlowFixMe
           return GraphQLInt;
         }
         return null;
@@ -37,7 +172,7 @@ describe('check how works GraphQLUnionType', () => {
       }),
     });
 
-    const res = await graphql({
+    const res: any = await graphql({
       schema,
       source: `
         query {
